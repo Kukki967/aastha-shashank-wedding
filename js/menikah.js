718fd32303d14a08b32ae42eb52ab132b9cbe86f
Kukki967 <42519710+Kukki967@users.noreply.github.com>
2021-10-04 18:45:56 +0530

    Add files via upload

---
+++ js/menikah.js
@@ -0,0 +1,82 @@
+// Get that hamburger menu cookin' //
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener("click", function() {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
+// Smooth Anchor Scrolling
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
+// When the user scrolls down 20px from the top of the document, show the scroll up button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("toTop").style.display = "block";
+  } else {
+    document.getElementById("toTop").style.display = "none";
+  }
+}
+
+// Preloader
+$(document).ready(function($) {
+  $(".preloader-wrapper").fadeOut(1000);
+  $("body").removeClass("preloader-site");
+});
+$(window).load(function() {
+  const Body = $("body");
+  Body.addClass("preloader-site");
+});
+
+
+const card = document.querySelector(".card");
+const title = document.querySelector(".title");
+const header = document.querySelector(".header img");
+const description = document.querySelector(".info h3");
+
+// Animate In
+
+card.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  title.style.transform = "translateZ(3px)";
+  header.style.transform = "translateZ(3px) rotateZ(0deg)";
+  description.style.transform = "translateZ(3px)";
+});
+
+// Animate Out
+
+card.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  title.style.transform = "translateZ(0px)";
+  header.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+});
